446dbcf51e31a8f790a2c2f53b2abe2c1202f706
Nicholas Ott <njo0004@auburn.edu>
2023-12-14 15:04:30 -0800

    ephem calc function added to common needs to be tested

--- src/common/include/common/common.h
+++ src/common/include/common/common.h
@@ -13,6 +13,45 @@ typedef Eigen::Matrix<double,3,1> vec_3_1;
 typedef Eigen::Matrix<double,32,3> mat_32_3;
 typedef Eigen::Matrix<double,32,27> mat_32_27;
 typedef Eigen::Matrix<double,1,27> vec_1_27;
+typedef Eigen::Matrix<double,6,1> vec_6_1;
+typedef Eigen::Matrix<double,7,1> vec_7_1;
+
+/*
+    To-Do:
+    1) Function to Calculate SV States
+*/
+
+/*
+    Order of Ephemeris
+    IODE_sf1
+    IODE_sf2
+    gpsWeek
+    t_oe
+    A
+    deltaN
+    M_0
+    e
+    omega
+    C_uc
+    C_us
+    C_rc
+    C_rs
+    C_ic
+    C_is
+    i_0
+    iDot
+    omega_O
+    omegaDot
+    IODC
+    t_oc
+    T_GD
+    a_f0
+    a_f1
+    a_f2
+
+*/
+
+
 
 namespace cpp_nav_filt
 {
@@ -23,11 +62,35 @@ namespace cpp_nav_filt
             Common();
             ~Common();
 
-            void passSvEphem(vec_1_27& ephem_in,const int& sv_in);
+            // Getter and Passer for 
+            void receiveSvEphem(vec_1_27& ephem_in,const int& sv_in);
+            void sendvEphem(vec_1_27& ephem_out,const int& desired_sv);
+
+            vec_7_1 sendSvStates(const int& sv_in,const double& transmit_time,const double& transit_time);
 
         private:
+            // GPS Constants
+            const double gps_pi = M_PI; // PI
+            const double omega_e_dot = pow(7.2921151467,-5); // rotation rate of earth
+            const double GM = pow(3.986005,14);  
+            const double F = pow(-4.442807633,-10);
+            const int half_week = 302400; // [s]
 
-            mat_32_27 sv_ephem;
+            mat_32_27 sv_ephem; // matrix of satellite ephemeris
+            vec_7_1 sv_state_; // is the pos and vel of a satellite we care about given by sv in
+
+            // Internal Variables
+            int desired_sv_;
+            double T_transmit_;
+            double T_transit_;
+            double dt_;
+            double tk_;
+            double time_;
+
+            // Internal Functions
+            void calcSvPVStates(vec_7_1& sv_state); // this is adapted from Dr. Bevly's provided class code
+            void setCurrentEphem(vec_1_27& ephem,const int& sv);
+            double checkT(double time);
 
         protected:
 
