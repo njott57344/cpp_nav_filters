a6cffbf19f9beb316f56160427869ef5004aabfe
Nicholas Ott <njo0004@auburn.edu>
2024-05-16 13:11:07 -0700

    keep having weird git issues, solution seems to be deleting and recloning entire repo

--- cpp_nav_filt_src/include/imu_mech.h
+++ cpp_nav_filt_src/include/imu_mech.h
@@ -20,21 +20,26 @@ namespace cpp_nav_filt
             // Getters (to class) and senders (from class)
             void getPos(vec_3_1& pos); // set position state (for init or hard reset)
             void getVel(vec_3_1& vel); // set velocity state (for init or hard reset)
-            void getAtt(vec_3_1& att); // set local tangent frame attitude state (for init or hard reset)
-            void getInertialMeasurements(vec_3_1& f_ib_b,vec_3_1& w_ib_b); // receive current inertial measurements
+            void getAtt(vec_3_1& att,vec_3_1& init_lla); // set local tangent frame attitude state (for init or hard reset)
+            void getInertialMeasurements(vec_3_1& f_ib_b,vec_3_1& w_ib_b,vec_3_1& fbias_ib_b,vec_3_1& wbias_ib_b); // receive current inertial measurements
 
             void sendPos(vec_3_1& pos); // send pos to outside class
             void sendVel(vec_3_1& vel); // send vel to outside class
             void sendAtt(vec_3_1& att); // send att to outside class
             void sendCbm(mat_3_3& Cbm); // send Cbm (rotation from body to mech frame) to outside class
 
-
         private:
             
             ImuMechanizationSettings settings_;
 
-            vec_9_1 xhat_prior_; // Estimate of PVA prior to propagation
-            vec_9_1 xhat_posterior_; // Estimate of PVA posterior to propagation
+            vec_3_1 pos_prior_;
+            vec_3_1 pos_posterior_;
+
+            vec_3_1 vel_prior_;
+            vec_3_1 vel_posterior_;
+
+            mat_3_3 C_bm_prior_;
+            mat_3_3 C_bm_posterior_;
 
             vec_3_1 f_ib_b_; // specific force of body wrt inertial frame in body frame
             vec_3_1 f_ib_m_; // specific force of body wrt inertial frame in mechanization frame 
@@ -47,6 +52,7 @@ namespace cpp_nav_filt
             bool att_is_init_;
             
             void mechanizePVA(); // function that runs mechanization
+            void checkInitStatus();
 
         protected:
     }; // end of class
--- cpp_nav_filt_src/src/imu_mech.cpp
+++ cpp_nav_filt_src/src/imu_mech.cpp
@@ -5,6 +5,11 @@ namespace cpp_nav_filt
     ImuMechanization::ImuMechanization(ImuMechanizationSettings& settings_in)
     {
         settings_ = settings_in;
+
+        state_is_init_ = false;
+        pos_is_init_ = false;
+        vel_is_init_ = false;
+        att_is_init_ = false;
     }
 
     ImuMechanization::~ImuMechanization()
@@ -14,24 +19,43 @@ namespace cpp_nav_filt
 
     void ImuMechanization::getPos(vec_3_1& pos)
     {
-
+        pos_posterior_ = pos;
+        pos_is_init_ = true;
+        checkInitStatus();
     }
 
+    // ================== Getters ================== //
     void ImuMechanization::getVel(vec_3_1& vel)
     {
-
+        vel_posterior_ = vel;
+        vel_is_init_ = true;
+        checkInitStatus();
     }
 
-    void ImuMechanization::getAtt(vec_3_1& att)
+    void ImuMechanization::getAtt(vec_3_1& att,vec_3_1& init_lla)
     {
-        
+        C_bm_posterior_ = eul2EcefDCM(att,init_lla);
+        att_is_init_ = true;
+        checkInitStatus();
     }
 
-    void ImuMechanization::getInertialMeasurements(vec_3_1& f_ib_b,vec_3_1& w_ib_b)
+    void ImuMechanization::getInertialMeasurements(vec_3_1& f_ib_b,vec_3_1& w_ib_b,vec_3_1& fbias_ib_b,vec_3_1& wbias_ib_b)
     {
+        f_ib_b_ = f_ib_b - fbias_ib_b;
+        w_ib_b_ = w_ib_b - wbias_ib_b;
+
+        if(state_is_init_)
+        {
+            mechanizePVA();
+        }
+        else
+        {
+            std::cout<<"WARNING: PVA State is Not Initialized"<<std::endl;
+        }
 
     }
 
+    // ===================== Senders ==================== //
     void ImuMechanization::sendPos(vec_3_1& pos)
     {
 
@@ -42,13 +66,19 @@ namespace cpp_nav_filt
 
     }
 
-    void sendAtt(vec_3_1& att)
+    void ImuMechanization::sendAtt(vec_3_1& att)
     {
 
     }
 
-    void sendCbm(mat_3_3& Cbm)
+    void ImuMechanization::sendCbm(mat_3_3& Cbm)
     {
         
+    }
+
+    // =================== Internal Functions =============== //
+    void ImuMechanization::checkInitStatus()
+    {
+        state_is_init_ = pos_is_init_ && vel_is_init_ && att_is_init_;
     }
 }// end of namespace
