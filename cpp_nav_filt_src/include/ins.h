09ae3276b3ce0f3c70f0f3492e9d77937ba8010f
Nicholas Ott <njo0004@auburn.edu>
2024-07-17 15:26:05 -0700

    restarting INS development

---
+++ cpp_nav_filt_src/include/ins.h
@@ -0,0 +1,28 @@
+#ifndef INS_H
+#define INS_H
+
+#include "cpp_nav_filt_lib.h"
+
+namespace cpp_nav_filt
+{
+
+    class Ins
+    {
+        public:
+            
+            Ins();
+            ~Ins();
+
+            /* convention: 
+                a get function is the class GETTING something
+                a send function is the class SENDING something
+            */
+
+           
+        private:
+
+        protected:
+
+    };
+
+}
